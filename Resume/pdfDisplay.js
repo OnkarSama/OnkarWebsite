53543a639a70d34468ec4ebf43f428e5f2b03f48
Onkar Dhillon <onkardhillon73@gmail.com>
2024-01-03 01:10:42 -0500

    Got Resume page looking nice on one page and a download button gotta work on css still.

--- Resume/pdfDisplay.js
+++ Resume/pdfDisplay.js
@@ -1,1 +1,8 @@
-PDFObject.embed("../Resume/Resume.pdf", "#resume");
+document.getElementById('downloadButton').addEventListener('click', function () {
+    var link = document.createElement('a');
+    link.href = 'Onkar Dhillon - Resume.pdf';
+    link.download = 'Onkar Dhillon - Resume.pdf';
+    link.dispatchEvent(new MouseEvent('click'));
+});
+
+
